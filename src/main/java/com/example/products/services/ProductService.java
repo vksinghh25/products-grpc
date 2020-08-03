dc436b50bb6d5e8ff6892a052f24851bf27b8718
vksinghh25 <vipkumar@expedia.com>
2020-08-03 17:02:49 +0530

    adding more stuff to IDL

--- src/main/java/com/example/products/services/ProductService.java
+++ src/main/java/com/example/products/services/ProductService.java
@@ -1,8 +1,6 @@
 package com.example.products.services;
 
-import com.example.gen.proto.ProductRequest;
-import com.example.gen.proto.ProductResponse;
-import com.example.gen.proto.ProductsGrpc;
+import com.example.gen.proto.*;
 import io.grpc.stub.StreamObserver;
 import org.lognet.springboot.grpc.GRpcService;
 
@@ -14,6 +12,21 @@ public class ProductService extends ProductsGrpc.ProductsImplBase {
         ProductResponse productResponse = ProductResponse.newBuilder().setName("How Google Works!")
                 .setDescription("The Google Story").build();
         responseObserver.onNext(productResponse);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getAllProducts(emptyParams request, StreamObserver<MultiProductResponse> responseObserver) {
+        Product good2Great = Product.newBuilder().setName("Good to Great").setDescription("Best Mgmt Book").build();
+        Product howGoogleWorks = Product.newBuilder().setName("How Google Works!").setDescription("The Google Story!").build();
+        Product tribeOfMentors = Product.newBuilder().setName("Tribe Of Mentors").setDescription("By Tim Ferris").build();
+
+        MultiProductResponse multiProductResponse = MultiProductResponse.newBuilder().addProducts(good2Great)
+                .addProducts(howGoogleWorks)
+                .addProducts(tribeOfMentors)
+                .build();
+
+        responseObserver.onNext(multiProductResponse);
         responseObserver.onCompleted();
     }
 }
